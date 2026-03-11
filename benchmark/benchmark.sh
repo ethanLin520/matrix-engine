1cfa6ffa517616ed3e75d59e8e3f8651f2bf20f1
Ethan Lin <eeeeethanl@gmail.com>
2026-03-11 00:35:09 -0500

    Benchmark

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -7,7 +7,7 @@ for thread in 1 2 4 8; do
     echo "Running with $thread threads..."
 
     echo "\n" >> multiply_benchmark_results.txt
-    ../build /multiply_benchmark 5 $thread >> multiply_benchmark_results.txt
+    ../build/multiply_benchmark 5 $thread >> multiply_benchmark_results.txt
 
     echo "\n" >> determinant_benchmark_results.txt
     ../build/determinant_benchmark 20 $thread >> determinant_benchmark_results.txt
