1c56216ee560da2f0e19244fd7582ece73c38ec4
Ethan Lin <eeeeethanl@gmail.com>
2026-03-10 21:02:08 -0500

    Simplify benchmark code

--- src/determinant.cpp
+++ src/determinant.cpp
@@ -1,77 +1,35 @@
 #include "ParallelOperation.hpp"
+#include "BenchmarkHelpers.hpp"
 
 #include <algorithm>
-#include <chrono>
 #include <cstdlib>
-#include <iomanip>
 #include <iostream>
 #include <random>
 #include <thread>
 
-using std::chrono::duration_cast;
-using std::chrono::high_resolution_clock;
-using std::chrono::microseconds;
-
 namespace matrix_engine {
-
-template<int R, int C>
-void fillRandom(Matrix<double, R, C> &m, std::mt19937_64 &rng) {
-    std::uniform_real_distribution<double> dist(-1.0, 1.0);
-    for (int i = 0; i < R; ++i) {
-        for (int j = 0; j < C; ++j) {
-            m(i, j) = dist(rng);
-        }
-    }
-}
 
 template<int N>
 void runDeterminantCase(int iters, size_t threads) {
     std::mt19937_64 rng(54321);
     Matrix<double, N> m;
     const DeterminantOperation determinantOp{};
-    fillRandom(m, rng);
+    benchmark::fillRandom(m, rng);
 
-    auto seqStart = high_resolution_clock::now();
-    double seqOut = 0;
-    for (int i = 0; i < iters; ++i) {
-        seqOut = determinantOp(SeqMode{}, m);
-    }
-    auto seqEnd = high_resolution_clock::now();
-
-    auto parStart = high_resolution_clock::now();
-    double parOut = 0;
-    LockExecutor executor(threads);
-    for (int i = 0; i < iters; ++i) {
-        parOut = determinantOp(ParMode{executor}, m);
-    }
-    auto parEnd = high_resolution_clock::now();
-
-    auto lockFreeStart = high_resolution_clock::now();
-    double lockFreeOut = 0;
-    LockFreeExecutor lockFreeExecutor(threads, 4 * static_cast<size_t>(N));
-    for (int i = 0; i < iters; ++i) {
-        lockFreeOut = determinantOp(ParMode{lockFreeExecutor}, m);
-    }
-    auto lockFreeEnd = high_resolution_clock::now();
-
-    volatile double sink = seqOut + parOut + lockFreeOut;
-    (void)sink;
-
-    const auto seqUs = duration_cast<microseconds>(seqEnd - seqStart).count();
-    const auto parUs = duration_cast<microseconds>(parEnd - parStart).count();
-    const auto lockFreeUs = duration_cast<microseconds>(lockFreeEnd - lockFreeStart).count();
-
-    std::cout << "N=" << std::setw(2) << N
-              << "  iters=" << std::setw(4) << iters
-              << "  seq(μs)=" << std::setw(10) << seqUs
-              << "  par(μs)=" << std::setw(10) << parUs
-              << "  lf(μs)=" << std::setw(10) << lockFreeUs
-              << "  par_speedup=" << std::fixed << std::setprecision(2)
-              << static_cast<double>(seqUs) / static_cast<double>(parUs)
-              << "x"
-              << "  lf_speedup="
-              << static_cast<double>(seqUs) / static_cast<double>(lockFreeUs)
-              << "x\n";
+    benchmark::BenchmarkRunner runner(
+        N, 2, iters, threads, 4 * static_cast<size_t>(N)
+    );
+    runner.run<double>(
+        [&]() {
+            return determinantOp(SeqMode{}, m);
+        },
+        [&](Executor &executor) {
+            return determinantOp(ParMode{executor}, m);
+        },
+        [&](Executor &executor) {
+            return determinantOp(ParMode{executor}, m);
+        }
+    );
 }
 
 template<int... Ns>
--- src/multiply.cpp
+++ src/multiply.cpp
@@ -1,28 +1,13 @@
 #include "ParallelOperation.hpp"
+#include "BenchmarkHelpers.hpp"
 
 #include <algorithm>
-#include <chrono>
 #include <cstdlib>
-#include <iomanip>
 #include <iostream>
 #include <random>
 #include <thread>
 
-using std::chrono::duration_cast;
-using std::chrono::high_resolution_clock;
-using std::chrono::microseconds;
-
 namespace matrix_engine {
-
-template<int R, int C>
-void fillRandom(Matrix<double, R, C> &m, std::mt19937_64 &rng) {
-    std::uniform_real_distribution<double> dist(-1.0, 1.0);
-    for (int i = 0; i < R; ++i) {
-        for (int j = 0; j < C; ++j) {
-            m(i, j) = dist(rng);
-        }
-    }
-}
 
 template<int N>
 void runCase(int iters, size_t threads) {
@@ -30,50 +15,21 @@ void runCase(int iters, size_t threads) {
     Matrix<double, N> a;
     Matrix<double, N> b;
     const MultiplyOperation multiplyOperation{};
-    fillRandom(a, rng);
-    fillRandom(b, rng);
+    benchmark::fillRandom(a, rng);
+    benchmark::fillRandom(b, rng);
 
-    auto seqStart = high_resolution_clock::now();
-    Matrix<double, N, N> seqOut;
-    for (int i = 0; i < iters; ++i) {
-        seqOut = multiplyOperation(SeqMode{}, a, b);
-    }
-    auto seqEnd = high_resolution_clock::now();
-
-    auto parStart = high_resolution_clock::now();
-    Matrix<double, N, N> parOut;
-    LockExecutor executor(threads);
-    for (int i = 0; i < iters; ++i) {
-        parOut = multiplyOperation(ParMode{executor}, a, b);
-    }
-    auto parEnd = high_resolution_clock::now();
-
-    auto lockFreeStart = high_resolution_clock::now();
-    Matrix<double, N, N> lockFreeOut;
-    LockFreeExecutor lockFreeExecutor(threads);
-    for (int i = 0; i < iters; ++i) {
-        lockFreeOut = multiplyOperation(ParMode{lockFreeExecutor}, a, b);
-    }
-    auto lockFreeEnd = high_resolution_clock::now();
-
-    volatile double sink = seqOut(0, 0) + parOut(0, 0) + lockFreeOut(0, 0);
-    (void)sink;
-
-    const auto seqUs = duration_cast<microseconds>(seqEnd - seqStart).count();
-    const auto parUs = duration_cast<microseconds>(parEnd - parStart).count();
-    const auto lockFreeUs = duration_cast<microseconds>(lockFreeEnd - lockFreeStart).count();
-
-    std::cout << "N=" << std::setw(4) << N
-              << "  iters=" << std::setw(4) << iters
-              << "  seq(μs)=" << std::setw(10) << seqUs
-              << "  par(μs)=" << std::setw(10) << parUs
-              << "  lf(μs)=" << std::setw(10) << lockFreeUs
-              << "  par_speedup=" << std::fixed << std::setprecision(2)
-              << static_cast<double>(seqUs) / static_cast<double>(parUs)
-              << "x"
-              << "  lf_speedup="
-              << static_cast<double>(seqUs) / static_cast<double>(lockFreeUs)
-              << "x\n";
+    benchmark::BenchmarkRunner runner(N, 4, iters, threads);
+    runner.run<Matrix<double, N, N>>(
+        [&]() {
+            return multiplyOperation(SeqMode{}, a, b);
+        },
+        [&](Executor &executor) {
+            return multiplyOperation(ParMode{executor}, a, b);
+        },
+        [&](Executor &executor) {
+            return multiplyOperation(ParMode{executor}, a, b);
+        }
+    );
 }
 
 template<int... Ns>
